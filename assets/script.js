15070b5cc07d403fed467e3be2a8ac32aca6a429
Akuru <anjalismith0529@gmail.com>
2022-04-07 14:38:34 -0400

    created base files

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// timeszones for day.js
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+
+
+
+
+//create a fetch inside of a function request to fetch the data from the weather API
