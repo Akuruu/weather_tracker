c3348cd6dfbe3c72f6e51df10ed997582a198537
Akuru <anjalismith0529@gmail.com>
2022-04-11 17:07:17 -0400

    appended forecast to HTML and started a search history function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
 //Global Variables
 const searchHistory = [];
-// const weatherApiUrl = "https://api.openweathermap.org/";
-// const geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
 const apiKey = "d9a9fecef476e14177165be4b90c7d62";
 var weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&current.weather&exclude=minutely,daily&current.temp&current.uvi&current.wind_speed&current.humidity&units=imperial&appid=${apiKey}`;
 var geoApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=Orlando&limi=5&appid=${apiKey}`;
@@ -22,10 +20,21 @@ let searchButton = document.querySelector("#search-button");
 
 
 //function to display search history
-//function displaySearchHistory (){
-// searchDisplayBox.innerHTML = "";
-// }
-
+function displaySearchHistory (){
+ searchDisplayBox.innerHTML = "";
+ for (let i = 0; i < searchHistory.length; i++ ) {
+     var last = document.createElement("input");
+     last.setAttribute("type","text");
+     last.setAttribute("readonly",true);
+     last.setAttribute("class", "form-control d-block bg-white");
+     last.setAttribute("value", searchHistory[i]);
+     last.addEventListener("click",function() {
+                displayWeather(last.value);
+            })
+            searchDisplayBox.append(last);
+    }
+}
+displaySearchHistory();
 
 // fetch for weather data
 function weatherData(weatherApiUrl) {
@@ -63,12 +72,13 @@ var selectedCity = {
 function retrieveGeo(data){
     selectedCity["name"] = data[0]["name"];
       // updateSearchHistory(selectedCity["name"]);
-      retrieveWeather([data[0]["lat"], data[0]["lon"]]);
+     retrieveWeather([data[0]["lat"], data[0]["lon"]]);
     
  }
   function retrieveWeather(latLon){
-    weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+latLon[0]+ "&lon=" + latLon[1]+ `&units=imperial&exclude=hourly&appid=${apiKey}`
-    weatherData(weatherApiUrl);
+     weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+latLon[0]+ "&lon=" 
+     + latLon[1]+ `&units=imperial&exclude=hourly&appid=${apiKey}`
+     weatherData(weatherApiUrl);
   }  
 
 //displays the weather
@@ -79,19 +89,25 @@ function displayWeather(data) {
     todayBox.append(todayList);
 
 // lists the forecasts for the week
-  var daily = data["daily"].slice(1, 5);
+
   var weekDay = 1;
-  weekDay++;
+  var daily = data["daily"].slice(0, 5);
+
+  
   for (day in daily){
     var weeklyWeather = document.createElement('div')
     forecastBox.append(weeklyWeather);
-    weeklyWeather.innerHTML = dayjs().add(weekDay, 'day').format("MM/DD/YYYY h:mm A") + " <img src='http://openweathermap.org/img/wn/" + daily[day]["weather"][0]["icon"] + "@2x.png' /></p>" +
+    weeklyWeather.innerHTML = dayjs().add(weekDay, 'day').format("MM/DD/YYYY h:mm A") + 
+    " <img src='http://openweathermap.org/img/wn/" + 
+    daily[day]["weather"][0]["icon"] + "@2x.png' /></p>" +
     "<p> Temp: " + daily[day]["temp"]["day"] + " F</p>" +
     "<p> UV Index: " + selectedCity["current"]["uvi"] + "</p>" +
     "<p> Wind: " + daily[day]["wind_speed"] + " MPH</p>" +
     "<p> Humidity: " + daily[day]["humidity"] + " %</p>";
     
-  }
+    //displays new date for each day
+    weekDay++;
+    }
  
 }
 
